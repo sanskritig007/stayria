c3aebfe3ede17209c8d7413241ed2af36e53f94b
unknown <sanskritig007@gmail.com>
2025-11-02 19:31:28 +0530

    Express sessions and connect-flash

--- classroom/server.js
+++ classroom/server.js
@@ -1,33 +1,82 @@
 const express= require("express");
 const app= express();
+const path=require("path");
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
-const cookieParser = require("cookie-parser");
+const session =require("express-session");
+const flash = require("connect-flash");
 
-app.use(cookieParser("secretcode"));
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
 
-app.get("/getsignedcookie",(req,res)=>{
-    res.cookie("made-in","India",{signed:true});
-    res.send("Signed cookie sent");
+const sessionOptions={
+        secret:"mysupersecretstring",
+        resave:false,
+        saveUninitialized : true
+        };
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req,res,next)=>{
+     res.locals.successMsg = req.flash("success");
+     res.locals.errorMsg = req.flash("error");
+     next();
+})
+app.get("/register",(req,res)=>{
+    let{name="anonymous"}=req.query;
+    req.session.name=name;
+    if(name === "anonymous"){
+         req.flash("error","User not registered");
+    }else{
+        req.flash("success","user registered successfully!!");
+    }
+    res.redirect("/hello");
 })
 
-app.get("/verify",(req,res)=>{
-    console.log(req.signedCookies);
-    res.send("verified");
+app.get("/hello",(req,res)=>{
+    res.render("page.ejs",{name: req.session.name});
 })
 
-app.get("/greet",(req,res)=>{
-    let{name = "anonymous"}=req.cookies;
-    res.send(`Hi,${name}`);
-})
 
-app.get("/",(req,res)=>{
-    console.dir(req.cookies);
-    res.send("Hi, I am root")
-})
+// app.get("/reqcount",(req,res)=>{
+//     if(req.session.count){   //single session ko count krta h aur ager ussi value ko track krne ke liye count variable bana liya h 
+//         req.session.count++;
+//     }else{
+//         req.session.count=1;
+//     }
+    
+//     res.send(`You sent a request ${req.session.count} times`);
+// })
 
-app.use("/users",users);
-app.use("/posts",posts);
+// app.get("/test",(req,res)=>{
+//     res.send("test successful!");
+// })
+// const cookieParser = require("cookie-parser");
+
+// app.use(cookieParser("secretcode"));
+
+// app.get("/getsignedcookie",(req,res)=>{
+//     res.cookie("made-in","India",{signed:true});
+//     res.send("Signed cookie sent");
+// })
+
+// app.get("/verify",(req,res)=>{
+//     console.log(req.signedCookies);
+//     res.send("verified");
+// })
+
+// app.get("/greet",(req,res)=>{
+//     let{name = "anonymous"}=req.cookies;
+//     res.send(`Hi,${name}`);
+// })
+
+// app.get("/",(req,res)=>{
+//     console.dir(req.cookies);
+//     res.send("Hi, I am root")
+// })
+
+// app.use("/users",users);
+// app.use("/posts",posts);
 
 app.listen(3000,()=>{
     console.log("server is listening to 3000");

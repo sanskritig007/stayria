d60512c34da987e0767176bcb44aadd07ffd8e0c
unknown <sanskritig007@gmail.com>
2025-11-17 20:01:30 +0530

    Added user model and authentication setup

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const ejsMate=require("ejs-mate");
 const ExpressError=require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStratrgy=require("passport-local");
+const User=require("./models/user.js");
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js")
@@ -45,11 +48,26 @@ app.get("/",(req,res)=>{//basic api call
 app.use(session(sessionOptions));
 app.use(flash());        //must be before routes calling because flash ko routes ke help se use krne wale h  
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStratrgy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next)=>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
+})
+
+app.get("/demouser",async(req,res)=>{
+    let fakeUser = new User({
+        email:"student@gmail.com",
+        username:"stayria-user"
+    });
+    let registeredUser= await User.register(fakeUser,"helloworld");
+    res.send(registeredUser);
 })
 
 app.use("/listings",listings);

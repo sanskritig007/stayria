6f758547c353f0fa732ee8a861aafdff8e6b04c2
unknown <sanskritig007@gmail.com>
2025-12-21 19:30:58 +0530

    Router-route compacting of code

--- routes/listing.js
+++ routes/listing.js
@@ -6,44 +6,27 @@ const {isLoggedIn, isOwner,validateListing}=require("../middleware.js")
 
 const listingController = require("../controllers/listings.js")
 
-
-
-//index route
-
-// router.post("/", wrapAsync(async (req, res) => {
-//     const newListing = new Listing(req.body.listing);
-//     await newListing.save();
-//     req.flash("success", "New Listing Created!");  // flash set here
-//     res.redirect("/listings");                     // redirect triggers next request
-// }));
-
-router.get(
-    "/",
-    wrapAsync(listingController.index) 
-); //hamare root route ke pass jaise hi GET req aaye waise hi index naam ka fuction ya jo call back h vo exicute hojye 
-
-//new route
-router.get("/new",isLoggedIn,listingController.renderNewForm);
-
-
-//show route
-router.get("/:id",wrapAsync(listingController.showListing));
-//create route
-router.post("/",
+//implementing Router.route ( to make it more compact)
+router
+ .route("/")
+ .get( wrapAsync(listingController.index) )//hamare root route ke pass jaise hi GET req aaye waise hi index naam ka fuction ya jo call back h vo exicute hojye 
+ .post(
     isLoggedIn,
     validateListing,
-    wrapAsync(listingController.createListing))
-//edit route
-// 
-router.get("/:id/edit",isLoggedIn,isOwner,wrapAsync(listingController.renderEditFrom))
-//update route
-// 
-router.put("/:id",
+    wrapAsync(listingController.createListing));//create
+
+//new route
+router.get("/new",isLoggedIn,listingController.renderNewForm);//id wale se uper rkhege warna router wala logic h usse id ki tarah interpret krega aur jo new wala route h usse database ke ander search krne ki kosis krega to error aaskta h .
+
+router.route("/:id")
+.get(wrapAsync(listingController.showListing))
+.put(
     isLoggedIn,
     isOwner,
     validateListing,
-    wrapAsync(listingController.updateListing));
-//delete route
-router.delete("/:id",isLoggedIn,isOwner,wrapAsync(listingController.destroyListing))
+    wrapAsync(listingController.updateListing))
+.delete(isLoggedIn,isOwner,wrapAsync(listingController.destroyListing))
 
+//edit route 
+router.get("/:id/edit",isLoggedIn,isOwner,wrapAsync(listingController.renderEditFrom))
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -7,15 +7,24 @@ const { isLoggedIn, saveRedirectUrl } = require("../middleware.js");
 
 const userController = require("../controllers/user.js");
 
-router.get("/signup",userController.renderSignupForm)
 
-router.post("/signup",wrapAsync(userController.signup))
+//router route
+router.route("/signup")
+.get(userController.renderSignupForm)
+.post(wrapAsync(userController.signup))
 
-router.get("/login",userController.renderLoginForm)
+// router.get("/signup",userController.renderSignupForm)
+// router.post("/signup",wrapAsync(userController.signup))
 
-router.post("/login",saveRedirectUrl,passport.authenticate("local",{ failureRedirect: "/login",failureFlash:true, }),
+router.route("/login")
+.get(userController.renderLoginForm)
+.post(saveRedirectUrl,passport.authenticate("local",{ failureRedirect: "/login",failureFlash:true, }),
 userController.login
 );
+
+// router.get("/login",userController.renderLoginForm)
+// router.post("/login",saveRedirectUrl,passport.authenticate("local",{ failureRedirect: "/login",failureFlash:true, }),
+// userController.login
 router.get("/logout",userController.logout)
 
 module.exports=router;

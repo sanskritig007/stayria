dc52021376a6833efaddc03e56ed2c08bbba61bb
unknown <sanskritig007@gmail.com>
2026-01-04 14:59:41 +0530

    Storage file

--- routes/listing.js
+++ routes/listing.js
@@ -6,7 +6,8 @@ const {isLoggedIn, isOwner,validateListing}=require("../middleware.js")
 
 const listingController = require("../controllers/listings.js")
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const {storage}=require("../cloudConfig.js")
+const upload = multer({ storage })
 
 
 //implementing Router.route ( to make it more compact)

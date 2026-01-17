b58e8789e6971edf0d3f540561b3bf1e9b82c10a
unknown <sanskritig007@gmail.com>
2026-01-17 20:14:25 +0530

    display image

--- init/data.js
+++ init/data.js
@@ -83,18 +83,6 @@ const sampleListings = [
     location: "Lake Tahoe",
     country: "United States",
   },
-  // {
-  //   title: "Luxury Penthouse with City Views",
-  //   description:
-  //     "Indulge in luxury living with panoramic city views from this stunning penthouse apartment.",
-  //   image: {
-  //     filename: "listingimage",
-  //     url: "https://images.unsplash.com/photo-1622396481328-9b1b78cdd9fd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2t5JTIwdmFjYXRpb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
-  //   },
-  //   price: 3500,
-  //   location: "Los Angeles",
-  //   country: "United States",
-  // },
   {
     title: "Ski-In/Ski-Out Chalet",
     description:

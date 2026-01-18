f70e57ba59ffe10bc61deea0b789e658705bc747
unknown <sanskritig007@gmail.com>
2026-01-18 12:34:31 +0530

    storing coordinates in database

--- controllers/listings.js
+++ controllers/listings.js
@@ -35,9 +35,7 @@ module.exports.createListing = async(req,res,next)=>{
      query: req.body.listing.location,
      limit: 1,
     })
-     .send()
-    console.log(response.body.features[0].geometry);
-    res.send("done!")
+     .send();
 
     let url= req.file.path;
     let filename= req.file.filename;
@@ -45,7 +43,9 @@ module.exports.createListing = async(req,res,next)=>{
     const newListing=new Listing(req.body.listing);
     newListing.owner = req.user._id;
     newListing.image={url,filename};
-    await newListing.save();
+    newListing.geometry= response.body.features[0].geometry;
+    let savedListing=await newListing.save();
+      console.log(savedListing)
     req.flash("success","New Listing Created!!")
     res.redirect("/listings");
 }
--- models/listing.js
+++ models/listing.js
@@ -30,6 +30,17 @@ const listingSchema=new Schema({
         type:Schema.Types.ObjectId,
         ref:"User",
         required:true,
+    },
+    geometry:{
+        type: {
+      type: String, // Don't do `{ location: { type: String } }`
+      enum: ['Point'], // 'location.type' must be 'Point'
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
     }
 })
 

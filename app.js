f987a287b9d95e6a72176b73ba464631160051c0
Sanskriti Goswami <sanskritig007@gmail.com>
2026-09-01 23:46:54 +0530

    locally working store

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 if(process.env.NODE_ENV != "production"){
     require('dotenv').config()
 }
-const port = process.env.PORT|| 8080;
+const port = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
